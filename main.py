10255f9d8880c95c5e091ac53bea3f59817fd894
Ernest Cheung <ErnestCheung02@gmail.com>
2020-12-06 13:15:50 -0800

    Completing functions, but bugs remain

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from random import choice
 
 
 class Account:
-    def __init__(self, name, wish, attempts=0, primogem=1600):
+    def __init__(self, name, wish, attempts=0, primogem=1600, characters):
         self.name = name
         self.wish = wish
         self.attempts = attempts
         self.primogem = primogem
+        self.characters = list()
 
     # This takes in the parameter "wish" in order to see which type of wish is chosen. Whichever is chosen, attempts is then sent to the method chosen.
     def choosewish(self, wish, attempts, primogem):
@@ -19,46 +20,58 @@ class Account:
         elif self.wish == 'Intertwined':
             return self.Intertwined(self.attempts, self.primogem)
 
+    def checkcharacters(self):
+        print(f'{self.name}')
+        print('The characters you currently own are')
+        print(f'{self.characters}')
     # This takes away 160 primogems from the user and runs the Intertwined wish
-    def Intertwined(self, attempts, primogems):
+
+    def Intertwined(self, attempts, primogem):
         for attempt in range(attempts):
             self.primogem -= 160
+            obj = IntertwinedPull(self.attempts)
+            data = obj.Intertwinedchances(self.attempts)
             # return self.Intertwinedpull(1)
 
     # This takes away 160 primogems from the user and runs the Acquaint wish
-    def Acquaint(self, attempts, primogems):
+    def Acquaint(self, attempts, primogem):
         for attempt in range(attempts):
             self.primogem -= 160
+            obj2 = AcquaintPull(self.attempts)
+            data2 = obj.Acquaintchances(self.attempts)
             # return self.AcquaintPull(1)
 
     class AcquaintPull(Account):
 
-        def __init__(self, chance, attempts):
-            self.chance = chance
+        def __init__(self, attempts):
             super(attempts)
 
-        # This takes in
-        def Acquaintchances(self, chance, attempts):
+        # This takes in attempts and uses the corresponding method from what is called for above. It will keep attempting and will print out what the result of each wish is
+        def Acquaintchances(self, attempts):
             for attempt in range(self.attempts):
-                self.chance = random.randrange(0, 10000, 3)
-                if self.chance >= 0 and self.chance <= 50:
+                chance = random.randrange(0, 10000, 3)
+                if chance >= 0 and chance <= 50:
                     five = ['Diluc', 'Jean', 'Keqing', 'Klee', 'Mona', 'Qiqi', 'Amos\' Bow', 'Lost Prayer to the Sacred Winds',
                             'Wolf\'s Gravestone', 'Skyward Blade', 'Skyward Harp', 'Skyward Atlas', 'Skyward Spine', 'Skyward Pride', 'Aquila Favonia']
                     random_five = choice(five)
-                    print(f'CONGRATULATIONS! YOU GOT A 5 STAR {random_five}.')
-                if self.chance >= 51 and self.chance <= 510:
+                    print(
+                        f'CONGRATULATIONS! YOU GOT A 5 STAR **{random_five}**.')
+                    return self.characters.append(random_five)
+                if chance >= 51 and chance <= 510:
                     four = ['Amber', 'Lisa', 'Kaeya', 'Barbara', 'Razor', 'Bennett', 'Noelle', 'Fischl',
                             'Sucrose', 'Beidou', 'Ningguang', 'Xiangling', 'Xingqiu', 'Chongyun', 'Diona', 'Xinyan']
                     random_four = choice(four)
-                    print(f'Nice! You got a 4 star {random_four}.')
+                    print(f'Nice! You got a 4 star *{random_four}*.')
+                    return self.characters.append(random_four)
                 else:
                     three = ['Sling shot', 'Raven Bow', 'Thrillingg Tales of Dragon Slayers', 'Black Tassel', 'Bloodtainted Greatsword',
                              'Skyrider Sword', 'Cool Steel', 'Sharpshooter\'s Oath', 'Emerald orb', 'Magic Guide', 'Debate Club', 'Ferrous Shadow', 'Harbinger of Dawn']
                     random_three = choice(three)
                     print(
                         f'Oof... You got a 3 star {random_three}. Better Luck next time!')
+                    return self.characters.append(random_three)
 
-        def Intertwinedchances(self):
+        def Intertwinedchances(self, attempts):
             for attempt in range(self.attempts):
                 self.chance = random.randrange(0, 10000, 3)
                 if self.chance >= 0 and self.chance <= 50:
@@ -67,29 +80,22 @@ class Account:
                     random_five2 = choice(five2)
                     print(
                         f'CONGRATULATIONS! YOU GOT A 5 STAR **{random_five2}**.')
+                    return self.characters.append(random_five)
                 if self.chance >= 51 and self.chance <= 510:
                     four2 = ['Amber', 'Lisa', 'Kaeya', 'Barbara', 'Razor', 'Bennett', 'Noelle', 'Fischl',
                              'Sucrose', 'Beidou', 'Ningguang', 'Xiangling', 'Xingqiu', 'Chongyun', 'Diona', 'Xinyan']
                     random_four2 = choice(four2)
                     print(f'Nice! You got a 4 star *{random_four2}*.')
+                    return self.characters.append(random_four)
                 else:
                     three2 = ['Sling shot', 'Raven Bow', 'Thrillingg Tales of Dragon Slayers', 'Black Tassel', 'Bloodtainted Greatsword',
                               'Skyrider Sword', 'Cool Steel', 'Sharpshooter\'s Oath', 'Emerald orb', 'Magic Guide', 'Debate Club', 'Ferrous Shadow', 'Harbinger of Dawn']
                     random_three2 = choice(three2)
                     print(
                         f'Oof... You got a 3 star {random_three2}. Better Luck next time!')
+                    return self.characters.append(random_three)
 
 
-    # def pull(self, wish):
-    #     if wish == 'Acquaint':
-    #         print('Pulled with Acquaint')
-    #     elif wish == 'Intertwined':
-    #         print('Pulled with Intertwined')
-    #         # Pull for Intertwined
-    #     else:
-    #         print(
-    #             'This is not a type of fate. Try again (Check your spelling and capitalization!)')
-# When you do
 if __name__ == "__main__":
     account_one = Account("acc1", 'Acquaint')
     accounttwo = Account("acc2", 'Intertwined')
---
+++ testing.py
@@ -0,0 +1,58 @@
+import random
+from random import choice
+
+
+def Acquaintchances(attempts):
+    for attempt in range(attempts):
+        chance = random.randrange(0, 10000, 3)
+        if chance >= 0 and chance <= 50:
+            five = ['Diluc', 'Jean', 'Keqing', 'Klee', 'Mona', 'Qiqi', 'Amos\' Bow', 'Lost Prayer to the Sacred Winds',
+                    'Wolf\'s Gravestone', 'Skyward Blade', 'Skyward Harp', 'Skyward Atlas', 'Skyward Spine', 'Skyward Pride', 'Aquila Favonia']
+            random_five = choice(five)
+            print(f'CONGRATULATIONS! YOU GOT A 5 STAR {random_five}.')
+
+        if chance >= 51 and chance <= 510:
+            four = ['Amber', 'Lisa', 'Kaeya', 'Barbara', 'Razor', 'Bennett', 'Noelle', 'Fischl',
+                    'Sucrose', 'Beidou', 'Ningguang', 'Xiangling', 'Xingqiu', 'Chongyun', 'Diona', 'Xinyan']
+            random_four = choice(four)
+            print(f'Nice! You got a 4 star {random_four}.')
+        else:
+            three = ['Sling shot', 'Raven Bow', 'Thrillingg Tales of Dragon Slayers', 'Black Tassel', 'Bloodtainted Greatsword',
+                     'Skyrider Sword', 'Cool Steel', 'Sharpshooter\'s Oath', 'Emerald orb', 'Magic Guide', 'Debate Club', 'Ferrous Shadow', 'Harbinger of Dawn']
+            random_three = choice(three)
+            print(
+                f'Oof... You got a 3 star {random_three}. Better Luck next time!')
+
+
+characters = []
+
+
+def Intertwinedchances(attempts):
+
+    for attempt in range(attempts):
+        chance = random.randrange(0, 10000, 3)
+        if chance >= 0 and chance <= 50:
+            five2 = ['Diluc', 'Jean', 'Keqing', 'Klee', 'Mona',
+                     'Qiqi', 'Tartaglia (Childe)', 'Venti', 'Xiao', 'Zhongli']
+            random_five2 = choice(five2)
+            print(
+                f'CONGRATULATIONS! YOU GOT A 5 STAR ** {random_five2} **.')
+            characters.append(random_five2)
+
+        if chance >= 51 and chance <= 510:
+            four2 = ['Amber', 'Lisa', 'Kaeya', 'Barbara', 'Razor', 'Bennett', 'Noelle', 'Fischl',
+                     'Sucrose', 'Beidou', 'Ningguang', 'Xiangling', 'Xingqiu', 'Chongyun', 'Diona', 'Xinyan']
+            random_four2 = choice(four2)
+            print(f'Nice! You got a 4 star * {random_four2} *.')
+            characters.append(random_four2)
+        else:
+            three2 = ['Sling shot', 'Raven Bow', 'Thrillingg Tales of Dragon Slayers', 'Black Tassel', 'Bloodtainted Greatsword',
+                      'Skyrider Sword', 'Cool Steel', 'Sharpshooter\'s Oath', 'Emerald orb', 'Magic Guide', 'Debate Club', 'Ferrous Shadow', 'Harbinger of Dawn']
+            random_three2 = choice(three2)
+            print(
+                f'Oof... You got a 3 star {random_three2}. Better Luck next time!')
+            characters.append(random_three2)
+
+
+Intertwinedchances(5)
+print(str(characters))
